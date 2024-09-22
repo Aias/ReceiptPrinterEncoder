c43293d2c1ddd4234cdeaf3e600dac574bf220c1
Nick Trombley <trombley.nick@gmail.com>
2024-09-22 10:07:33 -0400

    Type safety for text-wrap class

--- src/text-wrap.ts
+++ src/text-wrap.ts
@@ -1,116 +1,124 @@
+export interface TextWrapOptions {
+	indent?: number;
+	width?: number;
+	columns?: number;
+}
 
 /**
  * Wrap text into lines of a specified width.
  */
 class TextWrap {
-  /**
-     * Static function to wrap text into lines of a specified width.
-     *
-     * @param  {string}   value     Text to wrap
-     * @param  {object}   options   Object containing configuration options
-     * @return {array}              Array of lines
-     */
-  static wrap(value, options) {
-    const result = [];
-    let line = [];
-    let length = options.indent || 0;
-    const width = options.width || 1;
-    const columns = options.columns || 42;
+	/**
+	 * Static function to wrap text into lines of a specified width.
+	 *
+	 * @param  {string}   value     Text to wrap
+	 * @param  {object}   options   Object containing configuration options
+	 * @return {array}              Array of lines
+	 */
+	static wrap(value: string, options: TextWrapOptions) {
+		const chunkedLines: string[][] = [];
+		let line: string[] = [];
+		let length = options.indent || 0;
+		const width = options.width || 1;
+		const columns = options.columns || 42;
 
-    const lines = String(value).split(/\r\n|\n/g);
+		const lines = String(value).split(/\r\n|\n/g);
 
-    for (const value of lines) {
-      const chunks = value.match(/[^\s-]+?-\b|\S+|\s+|\r\n?|\n/g) || ['~~empty~~'];
+		for (const value of lines) {
+			const chunks = value.match(/[^\s-]+?-\b|\S+|\s+|\r\n?|\n/g) || ['~~empty~~'];
 
-      for (const chunk of chunks) {
-        if (chunk === '~~empty~~') {
-          result.push(line);
-          line = [];
-          length = 0;
-          continue;
-        }
+			for (const chunk of chunks) {
+				if (chunk === '~~empty~~') {
+					chunkedLines.push(line);
+					line = [];
+					length = 0;
+					continue;
+				}
 
-        /* The word does not fit on the line */
+				/* The word does not fit on the line */
 
-        if (length + (chunk.length * width) > columns) {
-          /* The word is longer than the line */
+				if (length + chunk.length * width > columns) {
+					/* The word is longer than the line */
 
-          if (chunk.length * width > columns) {
-            /* Calculate the remaining space on the line */
+					if (chunk.length * width > columns) {
+						/* Calculate the remaining space on the line */
 
-            const remaining = columns - length;
+						const remaining = columns - length;
 
-            /* Split the word into pieces */
+						/* Split the word into pieces */
 
-            const letters = chunk.split('');
-            let piece;
-            const pieces = [];
+						const letters = chunk.split('');
+						let piece;
+						const pieces = [];
 
-            /* If there are at least 8 position remaining, break early  */
+						/* If there are at least 8 positions remaining, break early  */
 
-            if (remaining > 8 * width) {
-              piece = letters.splice(0, Math.floor(remaining / width)).join('');
+						if (remaining > 8 * width) {
+							piece = letters.splice(0, Math.floor(remaining / width)).join('');
 
-              line.push(piece);
-              result.push(line);
+							line.push(piece);
+							chunkedLines.push(line);
 
-              line = [];
-              length = 0;
-            }
+							line = [];
+							length = 0;
+						}
 
-            /* The remaining letters can be split into pieces the size of the width */
+						/* The remaining letters can be split into pieces the size of the width */
 
-            while ((piece = letters.splice(0, Math.floor(columns / width))).length) {
-              pieces.push(piece.join(''));
-            }
+						while ((piece = letters.splice(0, Math.floor(columns / width))).length) {
+							pieces.push(piece.join(''));
+						}
 
-            for (const piece of pieces) {
-              if (length + (piece.length * width) >= columns) {
-                result.push(line);
-                line = [];
-                length = 0;
-              }
+						for (const piece of pieces) {
+							if (length + piece.length * width >= columns) {
+								chunkedLines.push(line);
+								line = [];
+								length = 0;
+							}
 
-              line.push(piece);
-              length += piece.length * width;
-            }
+							line.push(piece);
+							length += piece.length * width;
+						}
 
-            continue;
-          }
+						continue;
+					}
 
-          /* Word fits on the next line */
-          result.push(line);
-          line = [];
-          length = 0;
-        }
+					/* Word fits on the next line */
+					chunkedLines.push(line);
+					line = [];
+					length = 0;
+				}
 
-        /* Check if we are whitespace */
+				/* Check if we are whitespace */
 
-        if (chunk.match(/\s+/) && length == 0) {
-          continue;
-        }
+				if (chunk.match(/\s+/) && length == 0) {
+					continue;
+				}
 
-        line.push(chunk);
-        length += chunk.length * width;
-      }
+				line.push(chunk);
+				length += chunk.length * width;
+			}
 
-      if (line.length > 0) {
-        result.push(line);
-        line = [];
-        length = 0;
-      }
-    }
+			if (line.length > 0) {
+				chunkedLines.push(line);
+				line = [];
+				length = 0;
+			}
+		}
 
-    for (let i = 0; i < result.length; i++) {
-      result[i] = result[i].join('');
+		const result: string[] = [];
 
-      if (i < result.length - 1) {
-        result[i] = result[i].trimEnd();
-      }
-    }
+		for (let i = 0; i < chunkedLines.length; i++) {
+			let flattenedLine = chunkedLines[i].join('');
 
-    return result;
-  }
+			if (i < chunkedLines.length - 1) {
+				flattenedLine = flattenedLine.trimEnd();
+			}
+			result.push(flattenedLine);
+		}
+
+		return result;
+	}
 }
 
 export default TextWrap;
